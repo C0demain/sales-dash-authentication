f87b9e49e4faf5534fa2a63c95515d0f6ab44ad1
Érick Hideki Oyakawa Awata <erickhoawata@gmail.com>
2024-04-30 08:29:44 -0300

    arruma bugs product

--- src/router/ProductsRouter.ts
+++ src/router/ProductsRouter.ts
@@ -5,7 +5,7 @@ class ProductsRouter extends BaseRoutes{
     
     routes(): void {
         this.router.post("/register", ProductsController.register);
-        this.router.get("/getall", ProductsController.getProducts);
+        this.router.get("/getAll", ProductsController.getProducts);
         this.router.get('/:productId', ProductsController.getProduct)
         this.router.put('/:productId', ProductsController.updateProduct)
         this.router.delete('/:productId', ProductsController.deleteProduct)
