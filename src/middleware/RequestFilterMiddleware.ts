5d839eac59a01158687bc42bedb206edb4bf8906
Bruno Vieira <brunosilverio01@gmail.com>
2024-05-24 11:16:17 -0300

    Cria middlewares para filtros em req.query de usuário, cliente e produto
    userFilter -> { userId: ?}
    productFilter -> { productId: ?}
    clientFilter -> { clientId: ?}

---
+++ src/middleware/RequestFilterMiddleware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+
+export const userFilter = (req: any, res: any, next: NextFunction): any => {
+    const { userId } = req.query
+    let filters = req.filters || {}
+    if(userId){
+        filters = {...filters, userId: userId}
+        req.filters = filters
+    }
+    return next()
+}
+
+export const productFilter = (req: any, res: any, next: NextFunction): any => {
+    const { productId } = req.query
+    let filters = req.filters || {}
+    if(productId){
+        filters = {...filters, productId: productId}
+        req.filters = filters
+    }
+    return next()
+}
+
+export const clientFilter = (req: any, res: any, next: NextFunction): any => {
+    const { clientId } = req.query
+    let filters = req.filters || {}
+    if(clientId){
+        filters = {...filters, clientId: clientId}
+        req.filters = filters
+    }
+    return next()
+}
