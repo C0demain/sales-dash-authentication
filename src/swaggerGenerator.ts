3e575f223233ceb43ea83d727dd46bdfbfdddda9
Bruno Vieira <76965972+BrunoVieira003@users.noreply.github.com>
2024-06-11 09:09:44 -0300

    Renomeia arquivos swagger

--- src/swaggerGenerator.ts
+++ src/swaggerGenerator.ts
@@ -6,6 +6,20 @@ const doc = {
         description: 'API utilizada no projeto Sales Dash'
     },
     host: 'localhost:8000',
+    '@definitions': {
+        id: {type: 'number', example: 1},
+        userName: {type: 'string', example: 'John Doe'},
+        email: {type: 'string', example: 'johndoe@gmail.com'},
+        password: {type: 'string', example: '123'},
+        cpf: {type: 'string', example: '000123456789'},
+    },
+
+    definitions:{
+        AddSeller:{
+            $ref: '#/definitions/userName',
+        }
+    },
+
     components: {
         securitySchemes: {
             bearerAuth: {
@@ -17,7 +31,7 @@ const doc = {
     },
 };
 
-const outputFile = './swagger-output.json';
+const outputFile = './swagger.json';
 const routes = ['./index.ts'];
 
 swaggerAutogen({openapi: '3.0.0'})(outputFile, routes, doc);
